f7d581987739dbd5748d76fcbd9b4779a7755666
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2023-05-01 10:13:14 +0700

    Konversi Number (otomatis & manual)

--- src/KonversiNumber.java
+++ src/KonversiNumber.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class KonversiNumber {
+public class KonversiNumber {
+    public static void main(String[] args) {
+//        Widening Casting (Otomatis) : byte -> short -> int -> long -> float -> double
+//        Narrowing Casting (Manual) : double -> float -> long -> int -> char ->  short -> byte
+
+        //otomatis
+        byte iniByte = 10;
+        short iniShort = iniByte;;
+        int iniInt = iniShort;
+        long iniLong = iniInt;
+        float iniFloat = iniLong;
+        double iniDouble = iniFloat;
+
+        //manual
+        float iniFloat2 = (float) iniDouble;
+        long iniLong2 = (long) iniFloat2;
+        int iniInt2 = (int) iniLong2;
+        short iniShort2 = (short) iniInt2;
+        byte iniByte2 = (byte) iniShort2;
+    }
 }

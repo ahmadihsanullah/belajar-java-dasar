fcc50acf97241cdf50b4d048208158b52ad4c932
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2023-05-01 10:13:46 +0700

    Tipe Data Number

--- src/TipeDataNumber.java
+++ src/TipeDataNumber.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class TipeDataNumber {
+public class TipeDataNumber {
+    public static void main(String[] args) {
+//        tipe data number
+//       1. Integer number
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 10000;
+        long iniLong = 1000000;
+        long iniLong2 = 1000000L;
+
+//        2. Floating point
+        float iniFloat = 10.10F;
+        double iniDouble = 12.2121;
+
+//        decimal, hexadesimal dan binary
+        int decimalInt = 10;
+        int hexadecimalInt = 0xFFFFF;
+        int binaryInt = 0b10101;
+
+//        underscore untuk pemisah angka yang banyak
+        long amount = 1_000_000_000;
+        System.out.println(amount);
+
+    }
 }

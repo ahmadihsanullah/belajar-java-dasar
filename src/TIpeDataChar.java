a31a44fcf507550be361861783e0d96f9ea8947a
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2023-05-01 10:16:19 +0700

    Tipe Data Character

---
+++ src/TIpeDataChar.java
@@ -0,0 +1,16 @@
+public class TIpeDataChar {
+    public static void main(String[] args) {
+        char i = 'I';
+        char h = 'H';
+        char s = 'S';
+        char a = 'A';
+        char n = 'N';
+
+        System.out.print(i);
+        System.out.print(h);
+        System.out.print(s);
+        System.out.print(a);
+        System.out.print(n);
+
+    }
+}
